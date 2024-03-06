fac16c34b77d1c9d1b621ba3399908d74afa7b80
te22e-Olsson-Anton <anton.olsson3@elev.ga.ntig.se>
2024-03-06 14:58:07 +0100

    Riktig commit

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let cart = [];
 
+
 function lägg_till_varukorg(varu_namn, varu_pris) {
+
+    
+
+
+    
     let item = {
         namn: varu_namn,
         pris: varu_pris
     };
     cart.push(item);
+    
     visa_varukorg();
 }
 
@@ -20,4 +27,20 @@ function visa_varukorg() {
         cartElement.appendChild(itemName);
         cartElement.appendChild(itemPrice);
     });
+    visa_totalt_pris();
+}
+
+function visa_totalt_pris() {
+    let totalPris = 0;
+    cart.forEach(function(item) {
+        totalPris += item.pris;
+    });
+    let totalElement = document.getElementById('total');
+    totalElement.textContent = 'Totalt pris: ' + totalPris + ' €';
+}
+
+function rensa() {
+    cart.length = 0;
+    visa_varukorg();
+    visa_totalt_pris(); 
 }
